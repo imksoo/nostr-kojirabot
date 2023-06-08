1d610f5a08e4084c4832709965d3c41af435390d
Kirino Minato kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-06-08 15:30:49 +0000

    リプライを真面目にNIP-10に従った形にしたはず。。。

--- index.ts
+++ index.ts
@@ -24,11 +24,11 @@ pool.subscribe(
   [
     {
       kinds: [1],
-      since: Math.floor(new Date().getTime() / 1000 - 1 * 60),
+      since: Math.floor(new Date().getTime() / 1000 - 30 * 60),
     },
   ],
   feedRelays,
-  async (event, _isAfterEose, _relayURL) => {
+  (event, _isAfterEose, _relayURL) => {
     if (filterEvent(event)) {
       replyEvent(event);
     }
@@ -36,26 +36,63 @@ pool.subscribe(
 );
 
 function filterEvent(event: Nostr.Event): boolean {
-  return event.pubkey !== MyPubkey && event.content.includes("こじら");
+  if (event.pubkey === MyPubkey) {
+    return false;
+  }
+
+  if (event.content.match(/こじら[さん|氏]/)) {
+    return false;
+  } else if (event.content.match(/こじら[せ]/)) {
+    return true;
+  }
+  return false;
 }
 
 function replyEvent(event: Nostr.Event): void {
-  const reply = {
+  console.log(JSON.stringify({ event }));
+  const parsedReplyTags = Nostr.nip10.parse(event);
+  console.log(JSON.stringify({ parsedReplyTags }));
+
+  const tags: string[][] = [];
+  if (parsedReplyTags.root) {
+    tags.push(["e", parsedReplyTags.root.id, "", "root"]);
+    for (let i = 0; i < parsedReplyTags.mentions.length; ++i) {
+      const m = parsedReplyTags.mentions[i];
+      tags.push(["e", m.id, ""]);
+    }
+    tags.push(["e", event.id, "", "reply"]);
+  } else {
+    tags.push(["e", event.id, "", "root"]);
+    for (let i = 0; i < parsedReplyTags.mentions.length; ++i) {
+      const m = parsedReplyTags.mentions[i];
+      tags.push(["e", m.id, ""]);
+    }
+  }
+
+  for (let i = 0; i < parsedReplyTags.profiles.length; ++i) {
+    const p = parsedReplyTags.profiles[i];
+    tags.push(["p", p.pubkey]);
+  }
+  tags.push(["p", event.pubkey]);
+
+  console.log(JSON.stringify({ tags }));
+
+  const replyEvent = {
     sig: "",
     id: "",
     kind: 1,
-    tags: [...event.tags, ["e", event.id, "", "reply"], ["p", event.pubkey]],
+    tags,
     pubkey: MyPubkey ?? "",
     content: "呼んだ？",
     created_at: event.created_at + 1,
   };
 
-  reply.id = Nostr.getEventHash(reply);
-  reply.sig = Nostr.getSignature(reply, MySecret ?? "");
+  replyEvent.id = Nostr.getEventHash(replyEvent);
+  replyEvent.sig = Nostr.getSignature(replyEvent, MySecret ?? "");
 
-  const validateEvent = Nostr.validateEvent(reply);
-  const verifySignature = Nostr.verifySignature(reply);
-  console.log(JSON.stringify({ validateEvent, verifySignature, reply }));
+  const validateEvent = Nostr.validateEvent(replyEvent);
+  const verifySignature = Nostr.verifySignature(replyEvent);
+  console.log(JSON.stringify({ validateEvent, verifySignature, replyEvent }));
 
-  pool.publish(reply, feedRelays);
+  pool.publish(replyEvent, feedRelays);
 }
